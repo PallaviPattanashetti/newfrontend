7b065f56a610edd520303007d4d356f4208c4c01
Pallavi Pattanashetti <Pallavi.Pattanashetti@gmail.com>
2026-04-13 15:27:16 -0700

    nothing

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { MapLocationProvider } from "@/context/context";
 
 
 
@@ -29,7 +30,9 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-       
+       <MapLocationProvider>
+          {children}
+        </MapLocationProvider>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
