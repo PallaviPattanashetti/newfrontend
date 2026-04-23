79e9f2646cadd46218b23c4d1e839131eb364462
Pallavi Pattanashetti <Pallavi.Pattanashetti@gmail.com>
2026-04-23 14:12:29 -0700

    deleting commented page

--- context/context.tsx
+++ context/context.tsx
@@ -1,31 +1,3 @@
-// "use client";
-// import { MapLocation, MapLocationContextType } from "../interfaces/mapinterfaces";
-// import { useState, useContext, createContext, ReactNode } from "react";
-
-// const MapLocationContext = createContext<MapLocationContextType|undefined>(undefined);
-
-// export function MapLocationProvider({children}:{children:ReactNode})
-// {
-//     const [maplocation, setMapLocation]= useState<MapLocation|null>(null);
-//     return(
-//     <MapLocationContext.Provider value={{maplocation, setMapLocation}}>
-//         {children}
-//     </MapLocationContext.Provider>
-//     );
-// }
-
-// export function useMapLocation()
-// {
-
-//     const context = useContext(MapLocationContext);
-//     if(context===undefined)
-//     {
-//         throw new Error("useMapLocation must be used within a MapLocationProvider")
-//     }
-// return context;
-// }
-
-
 "use client";
 
 import React, { useState, useContext, createContext, ReactNode } from "react";
